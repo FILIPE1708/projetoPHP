8e6be9858e2397a0569813eb138fa89e6cae47b0
Filipe <lipecavalcante17@hotmail.com>
2021-10-21 21:36:07 -0300

    Novo exemplo

---
+++ Strings/exemplo2.php
@@ -0,0 +1,19 @@
+<?php
+
+$nome = 'filipe cavalcante';
+
+$nome = strtoupper($nome);
+echo $nome;
+
+$nome = strtolower($nome);
+echo '<br>';
+echo $nome;
+
+echo '<br>';
+echo ucwords($nome);
+
+
+echo '<br>';
+echo ucfirst($nome);
+
+?>

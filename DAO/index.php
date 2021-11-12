2fdfd1ac6d3bf52ddbe72e7475055ee279bdac37
Filipe <lipecavalcante17@hotmail.com>
2021-11-11 22:03:56 -0300

    Novos exemplos de busca

--- DAO/index.php
+++ DAO/index.php
@@ -4,11 +4,24 @@ require_once('config.php');
 
 /*$sql = new Sql;
 $user = $sql->select('SELECT * FROM tb_usuarios');
-echo json_encode($user);*/
+echo json_encode($user);
 
-$root = new Usuario();
+//Carrega um usuário
+/*$root = new Usuario();
 $root->loadById(3);
+echo $root;*/
 
-echo $root;
+//Carrega uma lista de usuários 
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+//Carrega uma lista de usuários buscando pelo login
+/*$busca = Usuario::searchList('ro');
+echo json_encode($busca);*/
+
+//Carrega o usuário se o login e a senha estiverem corretos
+$login = new Usuario();
+$login->login('Filipe', '3456788');
+echo ($login);
 
 ?>
